3b8776fa7876f8b2903a82bd38da28f5aff1d9cb
Yavor Kolev <yavorgeorgievkolev@gmail.com>
2024-02-09 11:19:22 +0200

    Update using from Class - ChangeColorOfNotch-StatusBar .java

--- ChangeColorOfNotch-StatusBar .java
+++ ChangeColorOfNotch-StatusBar .java
@@ -4,3 +4,17 @@ if (Build.VERSION.SDK_INT >= 21) {
     window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS); 
     window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark)); 
 }
+// or from Class
+public class Utils {
+
+    public void TaskBarChangeColor(Window window, int color){
+        if (Build.VERSION.SDK_INT >= 21) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(color);
+        }
+    }
+}
+//using in onCreate for example
+Utils utils = new Utils();
+utils.TaskBarChangeColor(this.getWindow(), this.getResources().getColor(R.color.colorBlack));
